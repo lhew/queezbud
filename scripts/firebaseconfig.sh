d4907498b87564eda104232987ae2c6728ccf48e
Leonardo Almeida Marques <leo@sydl1500661.oc.rabonet.com>
2025-07-26 23:53:38 +0200

    checking actions

--- scripts/firebaseconfig.sh
+++ scripts/firebaseconfig.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Usage: ./script.sh KEY1=VALUE1 KEY2=VALUE2 ...
+# Usage: Just run the script; it will use environment variables
 
-
-# Check if at least one argument is provided
-if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 KEY1=VALUE1 [KEY2=VALUE2 ...]"
-    exit 1
-fi
+# List of required Firebase config environment variables
+# List of required Firebase config environment variables (read from ENV)
+VARS=("API_KEY" "AUTH_DOMAIN" "PROJECT_ID" "STORAGE_BUCKET" "MESSAGING_SENDER_ID" "APP_ID")
 
 # Start the TypeScript file
 echo "export const firebaseConfig = {"
 
-# Loop through all arguments
-for input in "$@"; do
-    key="${input%%=*}"
-    value="${input#*=}"
-    echo "  $key: \"$value\","
+# Output each variable if set
+for key in "${VARS[@]}"; do
+    value="${!key}"
+    echo "  $key: \"${value//\"/\\\"}\","
 done
 
 # Close the object
